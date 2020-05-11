a32608ca956388f177ac1be3303b55a0a659dc90
Makai Spinks-Brown <mspinksbrown@gmail.com>
2020-05-11 09:10:34 -0700

    Organized code and swapped PIXI.Application with PIXI.renderer

--- public/js/game.js
+++ public/js/game.js
@@ -1,53 +1,93 @@
-//PIXI Settings
+//ANCHOR Pixi Settings
+/* -------------------------------------------------------------------------- */
+/*                            GLOBAL PIXI SETTINGS                            */
+/* -------------------------------------------------------------------------- */
+
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
-//Imports
+//ANCHOR Game Defaults
+/* -------------------------------------------------------------------------- */
+/*                                  DEFAULTS                                  */
+/* -------------------------------------------------------------------------- */
 
-const WIDTH = 1920;
-const HEIGHT = 1080;
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
 
-var movementSpeed = 2;
+//ANCHOR Renderer Settings
+/* -------------------------------------------------------------------------- */
+/*                                  RENDERER                                  */
+/* -------------------------------------------------------------------------- */
 
-var configuration = {
+let renderer = new PIXI.Renderer({
   width: WIDTH,
   height: HEIGHT,
   autoResize: true,
   resolution: window.devicePixelRatio,
   backgroundColor: 0xbbe773,
+});
+document.body.appendChild(renderer.view);
+let viewport = new PIXI.Container();
+
+//ANCHOR Ticker
+/* -------------------------------------------------------------------------- */
+/*                                   TICKER                                   */
+/* -------------------------------------------------------------------------- */
+
+let ticker = PIXI.Ticker.shared;
+ticker.add(function (delta) {
+  renderer.render(viewport);
+});
+
+//ANCHOR Network Variables
+/* -------------------------------------------------------------------------- */
+/*                               NETWORK OBJECTS                              */
+/* -------------------------------------------------------------------------- */
+
+let player, otherPlayer, socket;
+
+//ANCHOR Player Settings
+/* -------------------------------------------------------------------------- */
+/*                              PLAYER PARAMETERS                             */
+/* -------------------------------------------------------------------------- */
+
+var playerSettings = {
+  speed: 2,
+  zoom: 2,
+  direction: 0,
 };
 
-let player, playerSprite, otherPlayer, otherPlayerSprite, socket; //Sever objects
-
-let zoomFactor = 2,
-  direction = 0; //Client-side parameters
-
-let keys = {};
-
-let game = new PIXI.Application(configuration);
 let menu = new PIXI.Container();
-let viewport = new PIXI.Container();
-viewport.name = "viewport";
 
 let currentAnimation = 0;
 let anim;
 //viewport.scale.set(2);
 
-document.body.appendChild(game.view);
-game.renderer.resize(window.innerWidth, window.innerHeight);
-game.stage.addChild(viewport);
 //game.stage.addChild(viewport);
 
-preload();
+//ANCHOR Preload
+/* -------------------------------------------------------------------------- */
+/*                               PRELOAD ASSETS                               */
+/* -------------------------------------------------------------------------- */
 
-function preload() {
-  PIXI.Loader.shared
-    .add("idle", "../../assets/images/anims/idle.json")
-    .add("walk", "../../assets/images/anims/walk.json")
-    .add("LarcenyFont", "../../assets/fonts/Larceny/Larceny.xml.fnt")
-    .load(initialize);
-}
+PIXI.Loader.shared
+  .add("idle", "../../assets/images/anims/idle.json")
+  .add("walk", "../../assets/images/anims/walk.json")
+  .add("LarcenyFont", "../../assets/fonts/Larceny/Larceny.xml.fnt")
+  .load(initialize);
+
+//ANCHOR Initialize
+/* -------------------------------------------------------------------------- */
+/*                           INITIALIZE APPLICATION                           */
+/* -------------------------------------------------------------------------- */
 
 function initialize() {
+  var keys = {};
+
+  //ANCHOR Add players to Socket
+  /* -------------------------------------------------------------------------- */
+  /*                            ADD PLAYERS TO SERVER                           */
+  /* -------------------------------------------------------------------------- */
+
   var self = this;
   this.socket = io();
   socket = this.socket;
@@ -82,155 +122,161 @@ function initialize() {
     });
   });
 
-  //game.ticker.add(serverUpdate);
-}
+  //ANCHOR Input Function
+  /* -------------------------------------------------------------------------- */
+  /*                            DECLARE INPUT EVENTS                            */
+  /* -------------------------------------------------------------------------- */
 
-function input() {
-  window.addEventListener("keydown", keysDown);
-  window.addEventListener("keyup", keysUp);
+  function input() {
+    window.addEventListener("keydown", keysDown);
+    window.addEventListener("keyup", keysUp);
 
-  game.ticker.add(gameLoop);
-}
-
-function keysDown(e) {
-  console.log(e.keyCode);
-  keys[e.keyCode] = true;
-}
-
-function keysUp(e) {
-  keys[e.keyCode] = false;
-}
-
-function gameLoop(delta) {
-  player.vx = movementSpeed;
-  player.vy = movementSpeed;
-
-  if (keys["87"]) {
-    player.y -= player.vy;
-    direction = 1; //Up
-    currentAnimation = 1;
-  }
-  if (keys["83"]) {
-    player.y += player.vy;
-    direction = 3; //owm
-    currentAnimation = 1;
-  }
-  if (keys["65"]) {
-    player.x -= player.vx;
-    direction = 2; //Left
-    currentAnimation = 1;
-  }
-  if (keys["68"]) {
-    player.x += player.vx;
-    direction = 4; // Right
-    currentAnimation = 1;
-  }
-  if (!keys["87"] && !keys["83"] && !keys["65"] && !keys["68"]) {
-    debugCollision();
+    ticker.add(update);
   }
 
-  var x = player.x;
-  var y = player.y;
-  var r = player.rotation;
+  function keysDown(e) {
+    console.log(e.keyCode);
+    keys[e.keyCode] = true;
+  }
 
-  if (
-    player.oldPosition &&
-    (x !== player.oldPosition.x ||
-      y !== player.oldPosition.y ||
-      r !== player.oldPosition.rotation)
-  ) {
-    socket.emit("playerMovement", {
+  function keysUp(e) {
+    keys[e.keyCode] = false;
+  }
+
+  //ANCHOR Update (Tick)
+  /* -------------------------------------------------------------------------- */
+  /*                                UPDATE (TICK)                               */
+  /* -------------------------------------------------------------------------- */
+
+  function update(delta) {
+    player.vx = playerSettings.speed;
+    player.vy = playerSettings.speed;
+
+    if (keys["87"]) {
+      player.y -= player.vy;
+    }
+    if (keys["83"]) {
+      player.y += player.vy;
+    }
+    if (keys["65"]) {
+      player.x -= player.vx;
+    }
+    if (keys["68"]) {
+      player.x += player.vx;
+    }
+
+    var x = player.x;
+    var y = player.y;
+    var r = player.rotation;
+
+    if (
+      player.oldPosition &&
+      (x !== player.oldPosition.x ||
+        y !== player.oldPosition.y ||
+        r !== player.oldPosition.rotation)
+    ) {
+      socket.emit("playerMovement", {
+        x: player.x,
+        y: player.y,
+        rotation: player.rotation,
+      });
+    }
+    player.oldPosition = {
       x: player.x,
       y: player.y,
       rotation: player.rotation,
+    };
+    zoom();
+  }
+
+  //ANCHOR Add Players
+  /* -------------------------------------------------------------------------- */
+  /*                           ADD PLAYER TO VIEWPORT                           */
+  /* -------------------------------------------------------------------------- */
+
+  function addPlayer(self, playerInfo) {
+    let idle = PIXI.Loader.shared.resources["walk"].spritesheet;
+    let anim_idle = new PIXI.AnimatedSprite(idle.animations["walk"]);
+    anim_idle.animationSpeed = 0.75;
+    anim_idle.play();
+
+    playerSprite = new PIXI.Container();
+    playerSprite.scale.set(0.1);
+    playerSprite.addChild(anim_idle);
+
+    player = viewport.addChild(playerSprite);
+
+    const playerName = new PIXI.BitmapText(playerInfo.name, {
+      font: "100px Larceny",
+      align: "center",
     });
+
+    player.addChild(playerName);
+    player.x = playerInfo.x;
+    player.y = playerInfo.y;
+    player.vx = 0;
+    player.vy = 0;
+    player.pivot.x = 185 / 2;
+    player.pivot.y = 105 / 2;
+
+    input();
   }
-  player.oldPosition = {
-    x: player.x,
-    y: player.y,
-    rotation: player.rotation,
-  };
-  zoom();
 
-  //scroll();
-}
+  //ANCHOR Add Other Players
+  /* -------------------------------------------------------------------------- */
+  /*                        ADD OTHER PLAYERS TO VIEWPORT                       */
+  /* -------------------------------------------------------------------------- */
 
-function addPlayer(self, playerInfo) {
-  let idle = PIXI.Loader.shared.resources["walk"].spritesheet;
-  let anim_idle = new PIXI.AnimatedSprite(idle.animations["walk"]);
-  anim_idle.animationSpeed = 0.75;
-  anim_idle.play();
+  function addOtherPlayers(self, playerInfo) {
+    let idle = PIXI.Loader.shared.resources["walk"].spritesheet;
+    let anim_idle = new PIXI.AnimatedSprite(idle.animations["walk"]);
+    anim_idle.animationSpeed = 0.5;
+    anim_idle.play();
 
-  playerSprite = new PIXI.Container();
-  playerSprite.scale.set(0.1);
-  playerSprite.addChild(anim_idle);
+    otherPlayerSprite = new PIXI.Container();
+    otherPlayerSprite.scale.set(0.1);
+    otherPlayerSprite.addChild(anim_idle);
 
-  player = viewport.addChild(playerSprite);
+    const otherPlayer = self.otherPlayers.addChild(otherPlayerSprite);
+    otherPlayer.playerId = playerInfo.playerId;
+    otherPlayer.x = playerInfo.x;
+    otherPlayer.y = playerInfo.y;
 
-  const playerName = new PIXI.BitmapText(playerInfo.name, {
-    font: "100px Larceny",
-    align: "center",
-  });
+    viewport.addChild(otherPlayer);
+  }
 
-  player.addChild(playerName);
-  player.x = playerInfo.x;
-  player.y = playerInfo.y;
-  player.vx = 0;
-  player.vy = 0;
-  player.pivot.x = 185 / 2;
-  player.pivot.y = 105 / 2;
+  function zoom() {
+    const scaleDelta = 0.01;
 
-  input();
-}
+    offsetX = -(player.x * scaleDelta);
+    offsetY = -(player.y * scaleDelta);
 
-function addOtherPlayers(self, playerInfo) {
-  let idle = PIXI.Loader.shared.resources["walk"].spritesheet;
-  let anim_idle = new PIXI.AnimatedSprite(idle.animations["walk"]);
-  anim_idle.animationSpeed = 0.5;
-  anim_idle.play();
+    const currentScale = viewport.scale.x;
+    let nscale = currentScale + scaleDelta;
 
-  otherPlayerSprite = new PIXI.Container();
-  otherPlayerSprite.scale.set(0.1);
-  otherPlayerSprite.addChild(anim_idle);
-
-  const otherPlayer = self.otherPlayers.addChild(otherPlayerSprite);
-  otherPlayer.playerId = playerInfo.playerId;
-  otherPlayer.x = playerInfo.x;
-  otherPlayer.y = playerInfo.y;
-
-  viewport.addChild(otherPlayer);
-}
-
-function zoom() {
-  const scaleDelta = 0.01;
-
-  offsetX = -(player.x * scaleDelta);
-  offsetY = -(player.y * scaleDelta);
-
-  const currentScale = viewport.scale.x;
-  let nscale = currentScale + scaleDelta;
-
-  if (nscale < zoomFactor) {
-    playerSprite.pivot.x = 0;
-    playerSprite.pivot.y = 0;
-    viewport.position.x += offsetX;
-    viewport.position.y += offsetY;
-    viewport.scale.set(nscale);
+    if (nscale < playerSettings.zoom) {
+      playerSprite.pivot.x = 0;
+      playerSprite.pivot.y = 0;
+      viewport.position.x += offsetX;
+      viewport.position.y += offsetY;
+      viewport.scale.set(nscale);
+    }
   }
 }
-
-function debugCollision() {
-  playerRectX = playerSprite.getBounds().x;
-  playerRectY = playerSprite.getBounds().y;
-  playerRectW = playerSprite.getBounds().width;
-  playerRectH = playerSprite.getBounds().height;
-  var debug = new PIXI.Graphics();
-  debug.name = `debug`;
-  debug.alpha = 0.01;
-  debug.beginFill(0xffff00);
-  debug.lineStyle(1, 0xff0000);
-  debug.drawRect(playerRectX, playerRectY, playerRectW, playerRectH);
-  game.stage.addChild(debug);
+/*
+  function debugCollision() {
+    playerRectX = playerSprite.getBounds().x;
+    playerRectY = playerSprite.getBounds().y;
+    playerRectW = playerSprite.getBounds().width;
+    playerRectH = playerSprite.getBounds().height;
+    var debug = new PIXI.Graphics();
+    debug.name = `debug`;
+    debug.alpha = 0.01;
+    debug.beginFill(0xffff00);
+    debug.lineStyle(1, 0xff0000);
+    debug.drawRect(playerRectX, playerRectY, playerRectW, playerRectH);
+    game.stage.addChild(debug);
+  }
 }
 
 function serverUpdate(delta) {
@@ -240,7 +286,7 @@ function serverUpdate(delta) {
   });
 }
 
-/* scroll() {
+scroll() {
   const scrollDelta = 0.01;
 
   const offsetX = -(viewport.x * scrollDelta);
